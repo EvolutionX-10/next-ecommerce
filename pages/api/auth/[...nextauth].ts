939d3a14e5a5706983c27732b5587026b5f1d2c0
EvolutionX <evolutionx9777@gmail.com>
2023-05-26 12:15:30 +0530

    feat: add user authentication + prisma

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+	adapter: PrismaAdapter(prisma),
+	events: {
+		createUser: async ({ user }) => {
+			const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+				apiVersion: "2022-11-15",
+			});
+
+			if (user.email && user.name) {
+				const customer = await stripe.customers.create({
+					email: user.email,
+					name: user.name,
+				});
+
+				await prisma.user.update({
+					where: { id: user.id },
+					data: { stripeCustomerId: customer.id },
+				});
+			}
+		},
+	},
+});
