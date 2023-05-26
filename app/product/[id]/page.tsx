31de709704f2d5179cbcf9f25c0265746c4583e4
EvolutionX <evolutionx9777@gmail.com>
2023-05-26 20:08:51 +0530

    style: add prettier

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,21 +4,21 @@ import Image from "next/image";
 export default async function Product({ searchParams }: ProductPageProps) {
 	const { name, image, description, unit_amount } = searchParams;
 	return (
-		<div className="flex justify-between gap-24 p-12 text-gray-700">
+		<div className="flex flex-wrap justify-between gap-4 p-2 text-gray-700 sm:flex-nowrap md:gap-24 lg:p-12">
 			<Image
 				src={image}
 				alt={name}
 				height={800}
 				width={800}
-				className="w-full h-80 object-cover rounded-lg"
+				className="h-80 w-full rounded-lg object-cover"
 			/>
-			<div className="font-medium py-2">
+			<div className="py-2 font-medium">
 				<h1 className="text-2xl">{name}</h1>
 				<p className="text-gray-600">{description}</p>
 				<div className="flex gap-2">
 					<p className="font-bold text-teal-400">{PriceFormatter(+unit_amount)}</p>
 				</div>
-				<button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">
+				<button className="my-12 rounded-md bg-teal-700 px-6 py-2 font-medium text-white">
 					Add to Cart
 				</button>
 			</div>
