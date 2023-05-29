814550e9e8366a9d4b4a446febfcdcab0c83b531
EvolutionX <evolutionx9777@gmail.com>
2023-05-29 16:43:25 +0530

    feat: fonts!

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import type { Metadata } from "next";
 import Hydrate from "./components/Hydrate";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+	weight: ["400", "500", "700"],
+	subsets: ["latin-ext"],
+});
 
 export const metadata: Metadata = {
 	title: "Styled",
@@ -14,7 +20,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 	const session = await getServerSession(authOptions);
 
 	return (
-		<html lang="en">
+		<html lang="en" className={roboto.className}>
 			<body className="mx-8 min-h-screen lg:mx-60">
 				<Hydrate>
 					<Nav user={session?.user} expires={session?.expires ?? ""} />
